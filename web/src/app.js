399e5d421d4f20fe9321a8a86e0a70d46ba3a500
thanatkrit3ds <39578396+thanatkrit3ds@users.noreply.github.com>
2018-08-24 15:13:31 +0700

    Update app.js
    create route type get for generate short url.

--- web/src/app.js
+++ web/src/app.js
@@ -8,6 +8,8 @@ var MongoClient = require('mongodb').MongoClient;
 var mongoose = require('mongoose');
 
 shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$@');
+
+
 var connectString = 'mongodb://' + config.db.user+":"+config.db.pass + "@" + config.db.host + '/' + config.db.name;
 
 var Schema = mongoose.Schema;
@@ -58,6 +60,42 @@ app.post('/api/shorten', function(req, response){
       }
     );
   });
+
+});
+
+app.get('/api/shorten', function(req, response){
+
+    var longUrl = req.query.url;
+
+    MongoClient.connect(connectString, function(err, db) {
+        if (err){
+            response.send(err);
+            return;
+        }
+
+        var dbo = db.db(config.db.name);
+        var collection = dbo.collection("shorturl");
+
+        collection.find({url:longUrl}).toArray(function (err, items) {
+                if(items.length == 0){
+                    var shortId = shortid.generate();
+                    var urlData = { url:longUrl , short_id:shortId };
+                    collection.insertOne(urlData, function(err, res) {
+                        if (err) throw err;
+                        var url = config.webhost+shortId;
+                        // response.send({shortUrl:url});
+                        response.send(url);
+                        db.close();
+                    });
+                }else{
+                    var url = config.webhost+items[0].short_id;
+                    // response.send({shortUrl:url});
+                    response.send(url);
+                    db.close();
+                }
+            }
+        );
+    });
 
 });
 
