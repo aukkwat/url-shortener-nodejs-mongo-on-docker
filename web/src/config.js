83b7f1968d6b008adfd997b7ea6d9131ebdc21ba
thanatkrit3ds <39578396+thanatkrit3ds@users.noreply.github.com>
2018-08-24 15:14:09 +0700

    Update config.js
    setting environment variables

--- web/src/config.js
+++ web/src/config.js
@@ -1,11 +1,13 @@
 var config = {};
 
 config.db = {};
-config.webhost = 'http://localhost:3000/';
+// config.webhost = 'http://localhost:3000/';
 
-config.db.host = 'mongo';
-config.db.name = 'url_shortener';
-config.db.user = 'root';
-config.db.pass = 'example';
+config.webhost = process.env.LPN_WEB_HOST || 'http://localhost:3000/';
+
+config.db.host = process.env.LPN_MONGO_HOST || 'mongo';
+config.db.name = process.env.LPN_MONGO_DB || 'url_shortener';
+config.db.user = process.env.LPN_MONGO_USER || 'root';
+config.db.pass = process.env.LPN_MONGO_PASS || 'example';
 
 module.exports = config;
