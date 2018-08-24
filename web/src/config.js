ecb519dd41364689cf2afcd4ee471ebeca736acb
thanatkrit3ds <39578396+thanatkrit3ds@users.noreply.github.com>
2018-08-24 15:41:45 +0700

    Update config.js
    config port mongodb

--- web/src/config.js
+++ web/src/config.js
@@ -6,6 +6,7 @@ config.db = {};
 config.webhost = process.env.LPN_WEB_HOST || 'http://localhost:3000/';
 
 config.db.host = process.env.LPN_MONGO_HOST || 'mongo';
+config.db.port = process.env.LPN_MONGO_PORT || '27017';
 config.db.name = process.env.LPN_MONGO_DB || 'url_shortener';
 config.db.user = process.env.LPN_MONGO_USER || 'root';
 config.db.pass = process.env.LPN_MONGO_PASS || 'example';
